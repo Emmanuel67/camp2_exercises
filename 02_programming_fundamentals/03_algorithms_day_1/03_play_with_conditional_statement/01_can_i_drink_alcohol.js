0b69f83800f007ba1c6d6f096b6f78fd8ec17605
EMMANUEL CROCHARD <emmanuel.crochard@decathlon.com>
2017-11-24 14:54:31 +0100

    01_can_i_drink_alcohol OK

--- 02_programming_fundamentals/03_algorithms_day_1/03_play_with_conditional_statement/01_can_i_drink_alcohol.js
+++ 02_programming_fundamentals/03_algorithms_day_1/03_play_with_conditional_statement/01_can_i_drink_alcohol.js
@@ -1,12 +1,31 @@
 // > Frieda and Francis walk on the street. Frieda is 22 and Francis is 17.
-//
 // Create two objects, `francis`, `frieda` with the fields :
 //   `age` (an integer) and `name`, (obviously a string)
+
+const francis = {name: "Francis", age: 17};
+const frieda = {name: "Frieda", age: 22};
 
 // Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer,
 // respectively, to the question "Can he / she drink alcohol?".
 // Think about the Primitive Data Type that is best suited for the answer.
 // Hint: Think about the number of options
+
+let canFriedaDrinkAlcohol;
+if (frieda.age >= 18) {
+  canFriedaDrinkAlcohol = true;
+}
+else {
+  canFriedaDrinkAlcohol = false;
+}
+
+
+let canFrancisDrinkAlcohol;
+if (francis.age >= 18) {
+  canFrancisDrinkAlcohol = true;
+}
+else {
+  canFrancisDrinkAlcohol = false;
+}
 
 // These variables should stay correct if we modify the ages of the objects
 // `francis` and `frieda` in the declaration.
