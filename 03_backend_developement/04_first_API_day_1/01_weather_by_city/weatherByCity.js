79815278f45a1674f27e41a445c1c160bb1bc186
EMMANUEL CROCHARD <emmanuel.crochard@decathlon.com>
2017-12-07 14:46:53 +0100

    01 git commit -m01-

--- 03_backend_developement/04_first_API_day_1/01_weather_by_city/weatherByCity.js
+++ 03_backend_developement/04_first_API_day_1/01_weather_by_city/weatherByCity.js
@@ -2,16 +2,17 @@ const request = require("request");
 
 const API_KEY = process.env.OPENWEATHER_API_KEY;
 
-function weatherbycity(cityname) {
-  const myurl = url: `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&APPID=${API_KEY}`;
-
+function weatherbycity(cityName) {
+  const myUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&APPID=${API_KEY}`;
   const temp = request({
-    url:myurl,
+    url:myUrl,
     method: "GET"
   }, function(error, response, result) {
     const json = JSON.parse(result);
-    return ${json.main.temp} °C;
+    return `${json.main.temp} °C`;
+  });
+  return temp;
 }
 
-weatherbycity("Lille");
+//weatherbycity("Lille");
 module.exports = weatherbycity;
